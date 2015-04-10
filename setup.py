6d6631c80970f77acb4df25bf0098e27bba31eae
Fernando Flórez <fernando@funciton.com>
2015-04-10 18:27:42 -0500

    Updated setup to register on pypi

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 
 setup(
-    name='dict2xml',
-    version='0.1',
+    name='pydict2xml',
+    version='0.5.0',
     packages=['dict2xml'],
     author=u'Fernando Fl\xf3rez',
     author_email='fernando@funciton.com',
-    description='Small utility to convert a python dict into an xml tree.'
+    description='Small utility to convert a python dict into an xml tree.',
+    url='https://github.com/fernandoflorez/dict2xml'
 )
